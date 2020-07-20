1b164ab204700a6aed71e4d4722e52b37190a849
DanLaZ <lazenberry_daniel@yahoo.com>
2020-07-20 09:33:08 -0400

    Working on react counter example

---
+++ src/Components/Counter/Counter.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <h1></h1>
+    );
+
+};
+
+
+
+export default Counter;
